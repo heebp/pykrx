b0ffe3651dfb7037d5494d03b16a04622aec5a6a
heebp <91429968+heebp@users.noreply.github.com>
2024-09-28 01:31:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='pykrx',
     version=__version__,
     description='KRX data scraping',
-    url='https://github.com/heebp/pykrx',
+    url='https://github.com/sharebook-kr/pykrx',
     author='Brayden Jo, Jonghun Yoo',
     author_email=('brayden.jo@outlook.com, '
                   'jonghun.yoo@outlook.com, '
